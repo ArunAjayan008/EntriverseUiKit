b3717e2596d6e190a3d0191f402e6e9a48440b54
Jackson E J <jackson@entri.me>
2024-12-30 17:38:48 +0530

    removed entriverse-ui library for submodule

--- entriverse-ui/src/commonMain/kotlin/entriverse/shared/theme/typography/EntriverseTypographyProvider.kt
+++
@@ -1,7 +0,0 @@
-package entriverse.shared.theme.typography
-
-import entriverse.shared.theme.UserLocale
-import entriverse.shared.theme.dimen.EntriverseDimens
-
-fun interface EntriverseTypographyProvider:(UserLocale,EntriverseDimens)-> EntriverseTypography {
-}
